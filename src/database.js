ef1f0558493d5bc18cac7782316a96ebeb86f5b9
Dany <dany4ac@gmail.com>
2021-08-26 16:11:10 -0500

    Subiendo CRUD httpS 3.0

--- src/database.js
+++ src/database.js
@@ -1,9 +1,14 @@
 const mongoose = require("mongoose")
 
 
-mongoose.connect(process.env.MONGODB_URI, {
+/* mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true
+}) */
+
+mongoose.connect(`mongodb+srv://Fisi:Fisi123@cluster0.jridy.mongodb.net/Clase7?retryWrites=true&w=majority`, {
     useNewUrlParser: true
 })
+
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "error al conectar bd"));
 db.once("open", function () {
